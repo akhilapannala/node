156cca8df8e7f9e47a31880a03d34af22b6b4f2c
akhilapannala <pannala.akhila@gmail.com>
2018-05-20 18:14:41 +0530

    format

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ http.createServer(function (req, res) {
         res.end();
       });
  });
-  } else {
+  }
+   else {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="filetoupload"><br>');
